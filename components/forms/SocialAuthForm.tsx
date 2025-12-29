0b11ab845f15e47f309e5e42f6348455c5bdb1b0
MridulSingh7 <mridulsingh425.personal@gmail.com>
2025-12-30 00:23:29 +0530

    implemented the auth login using AUTH.JS and not NEXTAUTH.JS...

--- components/forms/SocialAuthForm.tsx
+++ components/forms/SocialAuthForm.tsx
@@ -1,11 +1,30 @@
+"use client";
 import React from "react";
 import { Button } from "../ui/button";
 import Image from "next/image";
+import { toast } from "sonner";
+import { signIn } from "next-auth/react";
+import ROUTES from "@/constants/routes";
+
+const handleSignIn = async (provider: "google" | "github") => {
+  try {
+    await signIn(provider, {
+      callbackUrl: ROUTES.HOME,
+    });
+  } catch (error) {
+    toast.error("Sign-in failed", {
+      description: error instanceof Error ? error.message : "Failed to sign-in",
+    });
+  }
+};
 
 const SocialAuthForm = () => {
   return (
     <div className="mt-10 flex flex-wrap gap-2.5">
-      <Button className="background-dark400_light900 body-medium text-dark200_light800 min-h-12 flex-1 px-4 py-3">
+      <Button
+        className="background-dark400_light900 body-medium text-dark200_light800 min-h-12 flex-1 px-4 py-3"
+        onClick={() => handleSignIn("github")}
+      >
         <Image
           src="/icons/github.svg"
           alt="github login"
@@ -15,7 +34,11 @@ const SocialAuthForm = () => {
         />
         <span>Login with Github</span>
       </Button>
-      <Button className="background-dark400_light900 body-medium text-dark200_light800 min-h-12 flex-1 px-4 py-3">
+
+      <Button
+        className="background-dark400_light900 body-medium text-dark200_light800 min-h-12 flex-1 px-4 py-3"
+        onClick={() => handleSignIn("google")}
+      >
         <Image
           src="/icons/google.svg"
           alt="google login"
