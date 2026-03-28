42ab12d37ea1a17f9fb4200678b2883eb0637fdc
MridulSingh7 <mridulsingh425.personal@gmail.com>
2026-03-28 07:38:23 +0530

    implemented tag specific questions

--- app/(root)/tags/page.tsx
+++ app/(root)/tags/page.tsx
@@ -3,20 +3,19 @@ import DataRenderer from "@/components/DataRenderer";
 import LocalSearch from "@/components/search/LocalSearch";
 import ROUTES from "@/constants/routes";
 import { EMPTY_TAGS } from "@/constants/states";
-import { getTags } from "@/lib/actions/tag.actions";
-
+import { getTags } from "@/lib/actions/tag.action";
 
 const Tags = async ({ searchParams }: RouteParams) => {
   const { page, pageSize, query, filter } = await searchParams;
 
-const { success, data, error } = await getTags({
+  const { success, data, error } = await getTags({
     page: Number(page) || 1,
     pageSize: Number(pageSize) || 10,
     query,
     filter,
-});
+  });
 
-const { tags } = data || {};
+  const { tags } = data || {};
   return (
     <>
       <h1 className="h1-bold text-dark100_light900 text-3xl">Tags</h1>
